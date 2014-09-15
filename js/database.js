b6a770ad52bb3675e0f0bf75151d530c6dbdba71
Zhai YiMing <tinymins@gmail.com>
2014-09-16 01:00:44 +0800

    基本完工 开始测试

--- js/database.js
+++ js/database.js
@@ -1,3 +1,111 @@
+function DB(name, version, display_name, size) {
+    var me = this ; // 把this保存下来，以后用_this代替this，这样就不会被this弄晕了
+    // private var
+    var db = window.openDatabase(name, version, display_name, size);
+    // public var
+    me.version = 1;
+    // privte functions
+    
+    // 类初始化
+    me.init = function() {
+        // 数据库初始化
+        db.transaction(function(tx) {
+            // tx.executeSql('DROP TABLE IF EXISTS topics');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS topics (id unique, name, version, data, enabled)');
+        }, function(err) {
+            alert("初始化数据库失败: " + err.code);
+        }, function() {
+            console.log("数据库初始化连接成功!");
+        });
+    };
+    me.init();
+    // 获取题库信息列表
+    me.list = function(callback) {
+        db.transaction(function(tx) {
+            tx.executeSql('SELECT id, name, version, enabled FROM topics', [], function(tx, results) {
+                var _result = [];
+                for (var i = 0; i < results.rows.length; i++) {
+                    _result.push(results.rows.item(i));
+                };
+                callback && callback(_result);
+            }, function(err) {
+                alert("获取题库信息列表失败: "+err.code);
+            });
+        }, function(err) {
+            console.error(err);
+        });
+    }
+    // 添加题库
+    me.insert = function(id, name, version, data) {
+        id = parseInt(id); version = parseInt(version);
+        db.transaction(function(tx) {
+            tx.executeSql('INSERT INTO topics (id, name, version, data, enabled) VALUES (?, ?, ?, ?, 1)', [ id, name, version, data ]);
+        }, function(err) {
+            console.error(err);
+        }, function() {
+            
+        });
+    };
+    // 更新题库
+    me.update = function(id, name, version, data) {
+        id = parseInt(id); version = parseInt(version);
+        db.transaction(function(tx) {
+            tx.executeSql('UPDATE topics SET name = ?, version = ?, data = ? WHERE id = ?', [ name, version, data, id ]);
+        }, function(err) {
+            console.error(err);
+        }, function() {
+            
+        });
+    };
+    // 删除题库
+    me.delete = function(id) {
+        id = parseInt(id);
+        db.transaction(function(tx) {
+            tx.executeSql('DELETE FROM topics WHERE id = ?', [ id ]);
+        }, function(err) {
+            console.error(err);
+        }, function() {
+            
+        });
+    }
+    // 启用题库
+    me.enable = function(id) {
+        id = parseInt(id);
+        db.transaction(function(tx) {
+            tx.executeSql('UPDATE topics SET enabled = 1 WHERE id = ?', [ id ]);
+        }, function(err) {
+            console.error(err);
+        }, function() {
+            
+        });
+    }
+    // 禁用题库
+    me.disable = function(id) {
+        id = parseInt(id);
+        db.transaction(function(tx) {
+            tx.executeSql('UPDATE topics SET enabled = 0 WHERE id = ?', [ id ]);
+        }, function(err) {
+            console.error(err);
+        }, function() {
+            
+        });
+    }
+    // 查询题库
+    me.select = function(id, callback) {
+        id = parseInt(id);
+        db.transaction(function(tx) {
+            tx.executeSql('SELECT data FROM topics WHERE id = ?', [ id ], function(tx, results) {
+                if ( results.rows.length > 0 )
+                    callback && callback(results.rows.item(0).data);
+            }, function(err) {
+                alert("获取题库信息列表失败: "+err.code);
+            });
+        }, function(err) {
+            console.error(err);
+        });
+    }
+}
+window.db = new DB("topic", "1.0", "topic_database", 1000000);
 // Wait for PhoneGap to load
 //
 document.addEventListener("deviceready", onDeviceReady, false);
@@ -5,33 +113,5 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // PhoneGap is ready
 //
 function onDeviceReady() {
-    function queryDB(tx) {
-        tx.executeSql('SELECT * FROM topics', [], querySuccess, errorCB);alert(1);
-    }
-
-    function querySuccess(tx, results) {
-        // this will be empty since no rows were inserted.
-        alert("Insert ID = " + results.insertId);
-        // this will be 0 since it is a select statement
-        alert("Rows Affected = " + results.rowAffected);
-        // the number of rows returned by the select statement
-        alert("Insert ID = " + results.rows.length);
-    }
-
-    function errorCB(err) {
-        alert("Error processing SQL: "+err.code);
-    }
     
-    function populateDB(tx) {
-        // tx.executeSql('DROP TABLE IF EXISTS topics');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS topics (id unique, name, version, data)');
-        tx.executeSql('INSERT INTO topics (id, name, version, data) VALUES (1, "测试题库", 0, \'[window.data = [{"type":"r","title":"\u8425\u4e1a\u7ec8\u4e86\uff0c\u67dc\u5458\u5bf9\u5f53\u65e5\u65e0\u6cd5\u6838\u9500\u7684\u8fc7\u6e21\u8d26\u9879\uff0c\u7ecf\u8fd0\u8425\u4e3b\u7ba1\u5ba1\u6279\uff0c\u767b\u8bb0\uff08 \uff09\u540e\uff0c\u4f5c\u6302\u8d26\u5904\u7406\u3002","keyword":"\u65e5\u7ec8\u5904\u7406","ans":["\u8fd0\u8425\u4e3b\u7ba1\u5de5\u4f5c\u65e5\u5fd7"]}]\')');
-    }
-
-    function successCB() {
-        alert("success!");
-    }
-    var db = window.openDatabase("topic", "1.0", "topic_database", 1000000);
-    db.transaction(populateDB, errorCB, successCB);
-    db.transaction(queryDB, errorCB);
 }
--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ $(window).load(function(){
     $("#input_search").keyup(function(){
         StartSearch($("#input_search").val());
     });
+    $(".btn-close-setting").live('mousedown touchstart', function() {
+        StartSearch($("#input_search").val());
+    });
     // 返回键
     var eventBackButton;
     eventBackButton = function() {
@@ -20,6 +23,111 @@ $(window).load(function(){
     document.addEventListener("menubutton", function() {
         $("#settingsModal").toggleClass('active');
     }, false);
+    // 加载题库数据
+    var loadTopicData = function() {
+        var mask = parseInt( Math.random() * 10000000 );
+        window.data = [];
+        window.dataMask = mask;
+        db.list(function(l) {
+            for (var i = 0; i < l.length; i++) {
+                if( l[i].enabled == 1 ) {
+                    db.select(l[i].id, function(data) {
+                        var topics = $.parseJSON(data);
+                        for (var i = 0; i < topics.length; i++) {
+                            if(window.dataMask == mask)
+                                window.data.push( topics[i] );
+                        };
+                    });
+                }
+            };
+        });
+    }
+    loadTopicData();
+    // 初始化设置面板
+    db.list(function(l) {
+        var getTopic = function(id) {
+            for (var i = l.length - 1; i >= 0; i--) {
+                if ( l[i].id == id ) {
+                    return l[i];
+                }
+            };
+        }
+        // 本地题库
+        var html = '';
+        for (var i = 0; i < l.length; i++) {
+            html += '<li class="table-view-cell" id="local_topic_' + l[i].id + '">' + EncodeHtml(l[i].name)
+                    + '<div class="toggle'+(l[i].enabled==1?' active':'')+'" data-id="' + l[i].id + '"><div class="toggle-handle"></div></div></li>';
+        };
+        $('.local-topic').html(html);
+        // 在线题库
+        $.getJSON("http://exam_searcher.jd-app.com/data.php", function(r) {
+            var html = "";
+            for (var i = 0; i < r.length; i++) {
+                var topic = getTopic(r[i].id);
+                var method, title, classname
+                if ( topic ) {
+                    if ( topic.version < r[i].version ) {
+                        title     = '更新';
+                        classname = 'btn-primary topic-update';
+                    } else {
+                        title     = '删除';
+                        classname = 'btn-negative topic-delete';
+                    }
+                } else {
+                    title     = '下载';
+                    classname = 'btn-positive topic-insert';
+                }
+                html += '<li class="table-view-cell">' + r[i].name +'<button class="btn topic-idu '+classname+'" data-id="' + r[i].id + '" data-id="' + r[i].id + '">'+title+'</button></li>';
+            };
+            $(".online-topic").html(html);
+            // 绑定按钮事件
+            $(".topic-idu").unbind().click(function() {
+                var me = this;
+                var id = $(me).data('id');
+                if ( $(me).hasClass('topic-insert') ) { // insert
+                    $(me).text('下载中').removeClass('btn-positive topic-insert');
+                    $.ajax({
+                        url : "http://exam_searcher.jd-app.com/data.php?id="+id,
+                        dataType : 'json',
+                        success : function(r) {
+                            db.insert(r.id, r.name, r.version, r.data);
+                            $('.local-topic').append('<li class="table-view-cell" id="local_topic_' + r.id + '">' + EncodeHtml(r.name) + '<div class="toggle active" data-id="' + r.id + '"><div class="toggle-handle"></div></div></li>');
+                            $(me).text('删除').addClass('btn-negative topic-delete');
+                        },
+                        error : function() {
+                            $(me).text('下载').addClass('btn-positive topic-insert');
+                        }
+                    });
+                } else if ( $(me).hasClass('topic-update') ) {
+                    $(me).text('下载中').removeClass('btn-primary topic-update');
+                    $.ajax({
+                        url : "http://exam_searcher.jd-app.com/data.php?id="+id,
+                        dataType : 'json',
+                        success : function(r) {
+                            db.insert(r.id, r.name, r.version, r.data);
+                            $(me).text('删除').addClass('btn-negative topic-delete');
+                        },
+                        error : function() {
+                            $(me).text('更新').addClass('btn-primary topic-update');
+                        }
+                    });
+                } else if ( $(me).hasClass('topic-delete') ) {
+                    db.delete(id);
+                    $('#local_topic_'+id).remove();
+                    $(me).text('下载').removeClass('btn-negative topic-delete').addClass('btn-positive topic-insert');
+                }
+            });
+        });
+    });
+    $('.local-topic')[0].addEventListener('toggle', function (e) {
+        var id = $(e.target).data('id');
+        if ( e.detail.isActive ) {
+            db.enable(id);
+        } else {
+            db.disable(id);
+        }
+        loadTopicData();
+    });
 });
 function EncodeHtml(s){
     return (typeof s != "string") ? s :
