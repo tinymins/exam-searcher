4e926e70e02d4ea617ed4513d07cd4a055c75f9f
Zhai YiMing <tinymins@gmail.com>
2014-09-16 12:54:14 +0800

    新增数据加载失败时重新加载按钮和加载中提示

--- js/index.js
+++ js/index.js
@@ -60,81 +60,95 @@ $(window).load(function(){
         }
         loadTopicData();
     });
-    db.list(function(l) {
-        var getTopic = function(id) {
-            for (var i = l.length - 1; i >= 0; i--) {
-                if ( l[i].id == id ) {
-                    return l[i];
-                }
-            };
-        }
-        // 本地题库
-        var html = '';
-        for (var i = 0; i < l.length; i++) {
-            html += '<li class="table-view-cell" id="local_topic_' + l[i].id + '">' + EncodeHtml(l[i].name)
-                    + '<div class="toggle'+(l[i].enabled==1?' active':'')+'" data-id="' + l[i].id + '"><div class="toggle-handle"></div></div></li>';
-        };
-        $('.local-topic').html(html);
-        // 在线题库
-        $.getJSON("http://exam_searcher.jd-app.com/data.php", function(r) {
-            var html = "";
-            for (var i = 0; i < r.length; i++) {
-                var topic = getTopic(r[i].id);
-                var method, title, classname
-                if ( topic ) {
-                    if ( topic.version < r[i].version ) {
-                        title     = '更新';
-                        classname = 'btn-primary topic-update';
-                    } else {
-                        title     = '删除';
-                        classname = 'btn-negative topic-delete';
+    var loadOnlineTopic;
+    loadOnlineTopic = function() {
+        db.list(function(l) {
+            var getTopic = function(id) {
+                for (var i = l.length - 1; i >= 0; i--) {
+                    if ( l[i].id == id ) {
+                        return l[i];
                     }
-                } else {
-                    title     = '下载';
-                    classname = 'btn-positive topic-insert';
-                }
-                html += '<li class="table-view-cell">' + r[i].name +'<button class="btn topic-idu '+classname+'" data-id="' + r[i].id + '" data-id="' + r[i].id + '">'+title+'</button></li>';
+                };
+            }
+            // 本地题库
+            var html = '';
+            for (var i = 0; i < l.length; i++) {
+                html += '<li class="table-view-cell" id="local_topic_' + l[i].id + '">' + EncodeHtml(l[i].name)
+                        + '<div class="toggle'+(l[i].enabled==1?' active':'')+'" data-id="' + l[i].id + '"><div class="toggle-handle"></div></div></li>';
             };
-            $(".online-topic").html(html);
-            // 绑定按钮事件
-            $(".topic-idu").unbind().click(function() {
-                var me = this;
-                var id = $(me).data('id');
-                if ( $(me).hasClass('topic-insert') ) { // insert
-                    $(me).text('下载中').removeClass('btn-positive topic-insert');
-                    $.ajax({
-                        url : "http://exam_searcher.jd-app.com/data.php?id="+id,
-                        dataType : 'json',
-                        success : function(r) {
-                            db.insert(r.id, r.name, r.version, r.data);
-                            $('.local-topic').append('<li class="table-view-cell" id="local_topic_' + r.id + '">' + EncodeHtml(r.name) + '<div class="toggle active" data-id="' + r.id + '"><div class="toggle-handle"></div></div></li>');
-                            $(me).text('删除').addClass('btn-negative topic-delete');
-                        },
-                        error : function() {
-                            $(me).text('下载').addClass('btn-positive topic-insert');
+            $('.local-topic').html(html);
+            // 在线题库
+            $(".online-topic").html('<li class="table-view-cell table-view-divider">加载中…</li>');
+            $.ajax({
+                url : "http://exam_searcher.jd-app.com/data.php",
+                dataType : 'json',
+                success : function(r) {
+                    var html = "";
+                    for (var i = 0; i < r.length; i++) {
+                        var topic = getTopic(r[i].id);
+                        var method, title, classname
+                        if ( topic ) {
+                            if ( topic.version < r[i].version ) {
+                                title     = '更新';
+                                classname = 'btn-primary topic-update';
+                            } else {
+                                title     = '删除';
+                                classname = 'btn-negative topic-delete';
+                            }
+                        } else {
+                            title     = '下载';
+                            classname = 'btn-positive topic-insert';
+                        }
+                        html += '<li class="table-view-cell">' + r[i].name +'<button class="btn topic-idu '+classname+'" data-id="' + r[i].id + '" data-id="' + r[i].id + '">'+title+'</button></li>';
+                    };
+                    $(".online-topic").html(html);
+                    // 绑定按钮事件
+                    $(".topic-idu").unbind().click(function() {
+                        var me = this;
+                        var id = $(me).data('id');
+                        if ( $(me).hasClass('topic-insert') ) { // insert
+                            $(me).text('下载中').removeClass('btn-positive topic-insert');
+                            $.ajax({
+                                url : "http://exam_searcher.jd-app.com/data.php?id="+id,
+                                dataType : 'json',
+                                success : function(r) {
+                                    db.insert(r.id, r.name, r.version, r.data);
+                                    $('.local-topic').append('<li class="table-view-cell" id="local_topic_' + r.id + '">' + EncodeHtml(r.name) + '<div class="toggle active" data-id="' + r.id + '"><div class="toggle-handle"></div></div></li>');
+                                    $(me).text('删除').addClass('btn-negative topic-delete');
+                                },
+                                error : function() {
+                                    $(me).text('下载').addClass('btn-positive topic-insert');
+                                }
+                            });
+                        } else if ( $(me).hasClass('topic-update') ) {
+                            $(me).text('下载中').removeClass('btn-primary topic-update');
+                            $.ajax({
+                                url : "http://exam_searcher.jd-app.com/data.php?id="+id,
+                                dataType : 'json',
+                                success : function(r) {
+                                    db.insert(r.id, r.name, r.version, r.data);
+                                    $(me).text('删除').addClass('btn-negative topic-delete');
+                                },
+                                error : function() {
+                                    $(me).text('更新').addClass('btn-primary topic-update');
+                                }
+                            });
+                        } else if ( $(me).hasClass('topic-delete') ) {
+                            db.delete(id);
+                            $('#local_topic_'+id).remove();
+                            $(me).text('下载').removeClass('btn-negative topic-delete').addClass('btn-positive topic-insert');
                         }
                     });
-                } else if ( $(me).hasClass('topic-update') ) {
-                    $(me).text('下载中').removeClass('btn-primary topic-update');
-                    $.ajax({
-                        url : "http://exam_searcher.jd-app.com/data.php?id="+id,
-                        dataType : 'json',
-                        success : function(r) {
-                            db.insert(r.id, r.name, r.version, r.data);
-                            $(me).text('删除').addClass('btn-negative topic-delete');
-                        },
-                        error : function() {
-                            $(me).text('更新').addClass('btn-primary topic-update');
-                        }
-                    });
-                } else if ( $(me).hasClass('topic-delete') ) {
-                    db.delete(id);
-                    $('#local_topic_'+id).remove();
-                    $(me).text('下载').removeClass('btn-negative topic-delete').addClass('btn-positive topic-insert');
+                }, 
+                error : function() {
+                    var html = '<li class="table-view-cell media"><a class="navigate-right"><span class="media-object pull-left icon icon-refresh"></span><div class="media-body">重新加载</div></a></li>';
+                    $(".online-topic").html(html);
+                    $(".online-topic").children('li').click(function() { loadOnlineTopic(); });
                 }
             });
         });
-    });
+    };
+    loadOnlineTopic();
     loadTopicData();
 });
 function EncodeHtml(s){
